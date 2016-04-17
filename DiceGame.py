b00de08feb7c02172606ad6eeebc5d9fb0e5a0e1
Keith Groves <keithalgroves@gmail.com>
2016-04-16 21:34:16 -0700

    add display dice to dice game

--- DiceGame.py
+++ DiceGame.py
@@ -1,17 +1,31 @@
 import random
 
+#holds the dice informaion
 dice = {}
+canvas = makeEmptyPicture(250,150)
 
 #initializes and resets the dice values
 def init():
   global dice
   dice = {"WIN" : [7, 11], "LOSE" : 0}
+  show(canvas)
 
 #creates and combines two random values from 1 - 6
 def roll():
     dice1 = random.randint(1,6)
+    showDie(dice1, 0)
     dice2 = random.randint(1,6)
+    showDie(dice2, 100)
     return dice1+dice2
+    
+    
+def showDie(i,  pos):
+  file = "C:\Users\Bob\Documents\CSUMB\Python\Labs\Lab15\dice"
+  file += `i`
+  file += ".PNG"
+  die = makePicture(file)
+  copyInto(makePicture(file), canvas, 1+pos,1)
+  repaint(canvas)
 
 #rolls the dice and returns true if the game congametinues, false if the game is won or lost
 def play():
